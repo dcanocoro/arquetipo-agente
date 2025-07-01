f12ad84f3d5d9aab66addca66ec388a1f4ccbe8f
Daniel Cano Coronado <dcanocor@ext.unicaja.es>
2025-07-01 11:54:33 +0200

    [QGDIAG-129] Desactivamos middleware para pruebas

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ from qgdiag_lib_arquitectura import LoggingMiddleware, init_error_handlers
 from qgdiag_lib_arquitectura.security import authentication
 
 app = FastAPI(title=settings.PROJECT_NAME, root_path="/qgdiag-microservicio-python-test")
-app.add_middleware(LoggingMiddleware)
+# app.add_middleware(LoggingMiddleware)
 app.include_router(call_orchestrator_route)
 init_error_handlers(app, context_name=settings.PROJECT_NAME)
 
